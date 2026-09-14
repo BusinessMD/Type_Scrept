7b998e1cad430fda3248f69cb811d4aa71904a9f
Ibrahim Mohamed <144424680+BusinessMD@users.noreply.github.com>
2026-09-14 03:17:58 +0300

    Update export.ts

--- modules/export.ts
+++ modules/export.ts
@@ -1,47 +1,42 @@
 import { sum } from "./modules/export.js";
 //* TYPES
-var name1 = "Ahmed";
-var age = 22;
+var name1: string = "Ahmed";
+var age: number = 22;
 //* UNION TYPE
-var id = 10;
+var id: number | string = 10;
 id = "ST-10";
 //* FUNCTION
-function multiply(x, y) {
-    return x * y;
+function multiply(x: number, y: number): number {
+  return x * y;
 }
-var student1 = { name: "Ali", age: 20 };
+//* INTERFACE
+interface Student {
+  name: string;
+  age: number;
+}
+var student1: Student = { name: "Ali", age: 20 };
 //* GENERIC
-function showData(data) {
-    return data;
+function showData<T>(data: T): T {
+  return data;
 }
 //* ENUM
-var Direction;
-(function (Direction) {
-    Direction[Direction["UP"] = 0] = "UP";
-    Direction[Direction["DOWN"] = 1] = "DOWN";
-    Direction[Direction["LEFT"] = 2] = "LEFT";
-    Direction[Direction["RIGHT"] = 3] = "RIGHT";
-})(Direction || (Direction = {}));
+enum Direction { UP, DOWN, LEFT, RIGHT }
 //* DECORATOR ---- SELF STUDY
 //* RECTANGLE
 class Rectangle {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
-    }
-    CalcCircumference() {
-        return 2 * (this.width + this.height);
-    }
+  constructor(public width: number, public height: number) {}
+  CalcCircumference(): number {
+    return 2 * (this.width + this.height);
+  }
 }
 var rectangle1 = new Rectangle(10, 5);
 console.log(name1, age, id);
 console.log(multiply(4, 5));
 console.log(student1);
-console.log(showData("Hello"));
+console.log(showData<string>("Hello"));
 console.log(Direction.UP);
 console.log(sum(5, 7));
 var result = document.getElementById("result");
 if (result) {
-    result.innerHTML = `Circumference: ${rectangle1.CalcCircumference()}<br>Thank YOU`;
+  result.innerHTML = `Circumference: ${rectangle1.CalcCircumference()}<br>Thank YOU`;
 }
-
